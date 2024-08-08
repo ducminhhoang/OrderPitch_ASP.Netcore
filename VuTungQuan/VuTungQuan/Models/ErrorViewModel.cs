0e6ed39276064f11c4d55a684dad6714d675d5b5
Đức Minh Hoàng <97384229+ducminhhoang@users.noreply.github.com>
2024-08-08 09:44:21 +0700

    Delete VuTungQuan directory

--- VuTungQuan/VuTungQuan/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace VuTungQuan.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
