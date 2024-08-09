935bb66ca8eee8d0a16f1c8f0c0e6ba0a169b3df
= <=>
2024-08-09 12:29:13 +0700

    Add project files.

---
+++ OrderFootballPitch/Models/Discount.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderFootballPitch.Models
+{
+    public class Discount
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 10)]
+        public string Code { get; set; }
+
+        [Required]
+        [Range(1, 100)]
+        public int Amount { get; set; }
+
+        [Range(1, Double.MaxValue)]
+        public Double UsageLimit { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}
