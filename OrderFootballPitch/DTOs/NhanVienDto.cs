0ff67d9d36bf11a13ca8e90f4abd30caab84dbb4
Đức Minh Hoàng <97384229+ducminhhoang@users.noreply.github.com>
2024-08-12 18:00:31 +0700

    Change DTOs

---
+++ OrderFootballPitch/DTOs/NhanVienDto.cs
@@ -0,0 +1,8 @@
+﻿public class NhanVienDto
+{
+    public int Id { get; set; }
+    public string HoTen { get; set; }
+    public string ChucVu { get; set; }
+    public string SoDienThoai { get; set; }
+    public string Email { get; set; }
+}
---
+++ OrderFootballPitch/DTOs/ThongTinThanhToanDto.cs
@@ -0,0 +1,9 @@
+﻿public class ThongTinThanhToanDto
+{
+    public int Id { get; set; }
+    public int KhachHangId { get; set; }
+    public int DatSanId { get; set; }
+    public DateTime NgayThanhToan { get; set; }
+    public decimal SoTien { get; set; }
+    public string PhuongThucThanhToan { get; set; }
+}
