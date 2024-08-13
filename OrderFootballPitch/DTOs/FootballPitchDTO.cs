2b905050bbb5ac584e5e5286886eb5dc5a2989a5
= <=>
2024-08-14 01:03:16 +0700

    Khôi phục các tệp bị xóa

---
+++ OrderFootballPitch/DTOs/FootballPitchDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace OrderFootballPitch.DTOs
+{
+    public class FootballPitchDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public TimeSpan TimeStart { get; set; }
+        public TimeSpan TimeEnd { get; set; }
+        public string Description { get; set; }
+        public double PricePerHour { get; set; }
+        public bool IsMaintenance { get; set; }
+        public int PitchTypeId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
