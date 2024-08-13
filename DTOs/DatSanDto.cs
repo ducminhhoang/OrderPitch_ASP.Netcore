fda77cf98ff355113960bba23fe56fb224d3b15d
Đức Minh Hoàng <97384229+ducminhhoang@users.noreply.github.com>
2024-08-14 00:51:32 +0700

    Delete DTOs directory

--- DTOs/DatSanDto.cs
+++
@@ -1,9 +0,0 @@
-﻿public class DatSanDto
-{
-    public int Id { get; set; }
-    public int KhachHangId { get; set; }
-    public int SanBongId { get; set; }
-    public DateTime ThoiGianBatDau { get; set; }
-    public DateTime ThoiGianKetThuc { get; set; }
-    public decimal TongTien { get; set; }
-}
