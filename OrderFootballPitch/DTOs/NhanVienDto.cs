ad1f0584cac51662bdbbdd2e1d541ddf8e034a54
Đức Minh Hoàng <97384229+ducminhhoang@users.noreply.github.com>
2024-08-16 13:41:27 +0700

    Delete OrderFootballPitch/DTOs directory

--- OrderFootballPitch/DTOs/NhanVienDto.cs
+++
@@ -1,8 +0,0 @@
-﻿public class NhanVienDto
-{
-    public int Id { get; set; }
-    public string HoTen { get; set; }
-    public string ChucVu { get; set; }
-    public string SoDienThoai { get; set; }
-    public string Email { get; set; }
-}
--- OrderFootballPitch/DTOs/YeuCauDto.cs
+++
@@ -1,8 +0,0 @@
-﻿public class YeuCauDto
-{
-    public int Id { get; set; }
-    public int KhachHangId { get; set; }
-    public string NoiDung { get; set; }
-    public DateTime NgayTao { get; set; }
-    public string TrangThai { get; set; }
-}
